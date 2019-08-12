41d3ae545b7c8fe2f520b73e6eb2be7c343df529
Dager <nasurov.eldar@mail.ru>
2019-08-12 12:24:52 +0300

    Updates

---
+++ precache-manifest.ce5d8501e0e9c29a9e6a3b1257cb3941.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79bc4dfce749edd5baca4083a6ea907b",
+    "url": "/Kanban/index.html"
+  },
+  {
+    "revision": "18f3eee012ecd17ec234",
+    "url": "/Kanban/static/css/main.70625b16.chunk.css"
+  },
+  {
+    "revision": "de37286d7760ae67a47b",
+    "url": "/Kanban/static/js/2.73bcb093.chunk.js"
+  },
+  {
+    "revision": "18f3eee012ecd17ec234",
+    "url": "/Kanban/static/js/main.1aa45901.chunk.js"
+  },
+  {
+    "revision": "958ccfa236bbc25c9ab0",
+    "url": "/Kanban/static/js/runtime~main.619b7780.js"
+  },
+  {
+    "revision": "6cb6e5fc57e9600d8f9f7cdfaa120d3d",
+    "url": "/Kanban/static/media/add.6cb6e5fc.svg"
+  },
+  {
+    "revision": "7bce26d56ccd2ae59725f43eccf3ac8b",
+    "url": "/Kanban/static/media/background.7bce26d5.jpg"
+  },
+  {
+    "revision": "5fe0db661a8f134c8ff7cb418373c469",
+    "url": "/Kanban/static/media/сlear.5fe0db66.svg"
+  }
+]);
